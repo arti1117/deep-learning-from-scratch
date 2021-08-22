5011dbc7178ce61b3c8b723b047de8a9e1d2c49c
arti <darkmoonz1004@gmail.com>
2021-08-22 17:26:35 +0900

    CHAPTER5 DONE

--- CHAPTER04/util.py
+++ CHAPTER04/util.py
@@ -14,8 +14,8 @@ def cross_entropy_error(y, t, one_hot_encode=False):
     if one_hot_encode:
         return -np.sum(t * np.log(y + delta)) / batch_size
     else:
-    	return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
-    	
+        return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
+        
 def numerical_diff(f, x):
     h = 1e-4
     return (f(x+h) - f(x-h)) / (2*h)
@@ -26,16 +26,22 @@ def numerical_gradient(f, x):
     grad = np.zeros_like(x)
     
     for idx in range(x.size):
-    	tmp_val = x[idx]
-    	
-    	x[idx] = tmp_val + h
-    	fxh1 = f(x)
-    	
-    	x[idx] = tmp_val - h
-    	fxh2 = f(x)
-    	
-    	grad[idx] = (fxh1 - fxh2) / (2*h)
-    	x[idx] = tmp_val
-    	
+        tmp_val = x[idx]
+        
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+        
     return grad
     
+def gradient_descent(f, init_x, lr=.01, step_num=100):
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
